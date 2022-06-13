e88308837e780c1d354568d3d393d0cc598b1c8c
mdmahir.mk <mdmahir.ds@gmail.com>
2022-06-13 15:17:57 +0600

    uload js practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+    const [ toggle, setToggle ] = useState(true);
+
+
+    const toggleBtn = () => {
+        if(toggle === true ){
+            setToggle(false)
+        }else{
+            setToggle(true)
+        }
+
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container mt-5">
+        <div>     
+               <button onClick={toggleBtn} className="btn-primary mt-3 btn">Toggle image</button>
+            {
+                toggle &&   <img
+                src="https://media-exp1.licdn.com/dms/image/C4D03AQFW_MHyKNxW7Q/profile-displayphoto-shrink_200_200/0/1653467346320?e=1660780800&v=beta&t=X71BA0p8MO7ETVu_Nrkh0oFF5Yk_rhLRxkxXNL2MV4s"
+                alt=""
+              />
+            }
+        </div>
+      </div>
     </div>
   );
 }
